035f357fc2d4a1cd16b1c0da7215e41395e3cc46
Z2426 <nguyenlehuy812@gmail.com>
2023-10-23 15:07:43 +0700

    connect database

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import morgan from "morgan"
+import bodyParser from "body-parser"
+import dbConnection from "./dbConfig/index.js"
+//securty packges
+import helmet from "helmet"
+dotenv.config()
+const app=express()
+const PORT =process.env.PORT || 8800
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json({limit:"10mb"}))
+app.use(express.urlencoded({extended: true}))
+app.use(morgan("dev"))
+dbConnection()
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${process.env.PORT}`)
+})
