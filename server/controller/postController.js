70f877d874cae2b8283f7c45083e92f860b361f5
Z2426 <nguyenlehuy812@gmail.com>
2023-10-30 17:34:39 +0700

    completed api post

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -1,5 +1,162 @@
 import Posts  from "../models/postModel.js"
 import Users from "../models/userModel.js"
+import Comments  from "../models/commentModel.js"
+export const deletePost =async(req,res,next)=>{
+    try{
+        const {id}=req.params
+        await Posts.findByIdAndDelete(id)
+        res.status(200).json({
+            success:true,
+            message:"Delete successfuly"
+        })
+    }catch(err){
+        console.log(err)
+        res.status(404).json({mesage:err.mesage})
+    }
+}
+export const replyPostComment =async(req,res,next)=>{
+    const {userId} =req.body.user
+    const {comment,replyAt,from} =req.body
+    const {id}=req.params
+    if(comment ===null){
+          return  res.status(404).json({message:"comment is required"})
+        }
+        
+    try{
+        const commentInfo =await Comments.findById(id)
+        commentInfo.replies.push({
+            comment,
+            replyAt,
+            from,
+            userId,
+            created_At:Date.now()
+
+        })
+        commentInfo.save()
+        res.status(200).json(commentInfo)
+
+    }catch(err){
+        console.log(err)
+        res.status(404).json({mesage:err.mesage})
+
+    }
+}
+export const commentPost =async (req,res,next)=>{
+    const {comment,from} =req.body
+        const {userId}= req.body.user
+        const {id} =req.params
+        if(comment===null){
+           return  res.status(404).json({comment,from,userId,postId:id})
+        }
+        const newComment =new Comments({comment,from,userId,postId:id})
+        await newComment.save()
+        //updateing the post with the comment id
+        const post =await Posts.findById(id)
+        post.comments.push(newComment._id)
+        const updatePost =await Posts.findByIdAndUpdate(id,post,{
+            new:true
+        })
+        res.status(201).json(newComment)
+}
+export const likePostComment =async(req,res,next)=>{
+   const {userId}=req.body.user
+   const {id,rid}=req.params
+   try{
+    if(rid===undefined || rid===null ||rid===`false`){
+        const comment =await Comments.findById(id)
+        const index=comment.likes.findIndex((el)=>el===String(userId))
+        if(index ===-1){
+            comment.likes.push(userId)
+        }else{
+            comment.likes =comment.likes.filter((i)=>i !==String(userId))
+        }
+        const updated =await Comments.findByIdAndUpdate(id,comment,{
+            new :true
+        })
+        res.status(201).json(updated)
+    }else{
+        const replyComments =await Comments.findOne(
+            {_id:id},
+            {
+                replies:{
+                    $elemMatch:{
+                        _id:rid
+                    }
+                }
+            }
+        )
+        const index =replyComments?.replies[0]?.likes.findIndex(
+            (i)=>i ===String(userId)
+        )
+        if(index ===-1){
+            replyComments.replies[0].likes.push(userId)
+        }else{
+            replyComments.replies[0].likes=replyComments.replies[0]?.likes.filter(
+                (i)=> i !==String(userId)
+            )
+        }
+        const query={_id:id,"replies._id":rid}
+        const updated={
+            $set:{
+                "replies.$likes":replyComments.replies[0].likes
+            }
+        }
+        const result =await Comments.updateOne(query,updated,{
+            new:true
+        })
+        res.status(201).json(result)
+    }
+
+   }catch(err){
+    console.log(err)
+    res.status(404).json({message:err.mesage})
+   }
+}
+export const likePost  =async (req,res,next)=>{
+    try{
+        const {userId} =req.body.user
+        const {id} =req.params
+        const post =await Posts.findById(id)
+        const index =post.likes.findIndex((pid)=>pid===String(userId))
+        if(index===-1){
+            post.likes.push(userId)
+        }else{
+            post.likes=post.likes.filter((pid)=>pid!==String(userId))
+
+        }
+        const newPost =await Posts.findByIdAndUpdate(id,post,{
+            new:true
+        })
+        res.status(200).json({
+            success:true,
+            message:"successfully",
+            data: newPost
+        })
+
+    }catch(err){
+      console.log(err)
+      res.status(404).json({message:err.mesage})
+    }
+}
+export const getComments =async(req,res,next)=>{
+    try{
+        const {postId} =req.params
+        const postComments =await Comments.find({postId})
+        .populate({
+            path:"userId",
+            select:"firstName lastName location profileUrl -password"
+        }).sort({_id:-1})
+        res.status(200).json({
+            success:true,
+            message:"succesfully",
+            data: postComments
+        })
+
+    }catch(err){
+      console.log(err)
+      res.status(404).json({message:err.mesage})
+    }
+}
 export const getUserPost =async(req,res,next)=>{
     try{
         const {id} =req.params
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,19 @@
 import  express from "express"
-import {createPost,getPost,getPosts,getUserPost} from "../controller/postController.js"
+import {createPost,getPost,getPosts,
+    getUserPost,getComments,likePost,
+    likePostComment,replyPostComment,
+    commentPost,deletePost} from "../controller/postController.js"
 import {userAuth} from "../middleware/authMiddleware.js"
  const router =express.Router()
+ //delete post
+ router.delete("/:id",userAuth,deletePost)
+ //like and comment on post
+ router.post("/like/:id",likePost)
+ router.post("/like-comment/:id/:rid?",userAuth,likePostComment)
+ router.post("/reply-commnet/:id",userAuth,replyPostComment)
+ router.post("/comment/:id",userAuth,commentPost)
+ //get comments
+ router.get("/comments/:postId",getComments)
  //get post
  router.post("/get-user-post/:id",userAuth,getUserPost)
  router.post("/:id",userAuth,getPost)
