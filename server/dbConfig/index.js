b952835e48b88acbb8e8619217aba396f486adcb
Z2426 <nguyenlehuy812@gmail.com>
2023-11-14 16:39:53 +0700

    page resgister: client + server /until/function

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -3,7 +3,7 @@ const dbConnection =async()=>{
     try{
         const connection =await mongoose.connect(process.env.MONGODB_URL,{
             useNewUrlParser:true,
-            // ...
+            
         })
         console.log("DB Connected Succesfully")
 
