edc5de50ce617ed3e780d99549bef510bc19118e
lehuy1121 <106235962+Z2426@users.noreply.github.com>
2023-12-11 11:01:20 +0700

    fix login : status & message

--- server/controller/autheController.js
+++ server/controller/autheController.js
@@ -29,47 +29,64 @@ export const register = async (req, res, next) => {
 
 }
 export const login = async (req, res, next) => {
-    const { email, password } = req.body
-    try {
-        if (!email || !password) {
-            next("Please Provide User credentials")
-            return
-        }
-        const user = await Users.findOne({ email })
-            .select('+password')
-            .populate({
-                path: "friends",
-                select: "firstName lastName location profileUrl -password"
-            });
-
-        if (!user) {
-            next("Inalid email or password")
-            return
-        }
-        if (!user?.verified) {
-            next("User email is not verified.chekc your email account and verify your email")
-            return
-        }
-        const isMatch = await compareString(password, user?.password)
-        if (!isMatch) {
-            next("Invalid email or password")
-            return
-        }
-        user.password = undefined
-        const token = createJWT(user?._id)
-        res.status(201).json({
-            success: true,
-            message: "Login successfully",
-            user,
-            token,
-            status:"success"
-        })
-
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({ 
-            message: error.message ,
-            status:"error"
-        })
+    const { email, password } = req.body;
+try {
+    if (!email || !password) {
+        res.status(400).json({
+            success: false,
+            message: "Please Provide User credentials",
+            status: "failed"
+        });
+        return;
     }
+
+    const user = await Users.findOne({ email }).select('+password').populate({
+        path: "friends",
+        select: "firstName lastName location profileUrl -password"
+    });
+
+    if (!user) {
+        res.status(401).json({
+            success: false,
+            message: "Invalid email or password",
+            status: "failed"
+        });
+        return;
+    }
+
+    if (!user.verified) {
+        res.status(403).json({
+            success: false,
+            message: "User email is not verified. Check your email account and verify your email",
+            status: "failed"
+        });
+        return;
+    }
+
+    const isMatch = await compareString(password, user.password);
+    if (!isMatch) {
+        res.status(401).json({
+            success: false,
+            message: "Invalid email or password",
+            status: "failed"
+        });
+        return;
+    }
+
+    user.password = undefined;
+    const token = createJWT(user._id);
+    res.status(201).json({
+        success: true,
+        message: "Login successfully",
+        user,
+        token,
+        status: "success"
+    });
+} catch (error) {
+    console.log(error);
+    res.status(500).json({
+        message: error.message,
+        status: "failed"
+    });
+}
 }
